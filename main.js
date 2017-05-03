3155279ead1656fe433e3a12b7570df5d98b519c
Matt Hambro <matt@Matthews-MacBook-Pro.local>
2017-05-02 23:56:41 -0400

    added loading gif, changed to 3 column layout

--- main.js
+++ main.js
@@ -3,20 +3,38 @@ var twitchFaves = ["ESL_SC2", "drdisrespectlive", "bloodyfaster", "OgamingSC2", 
 
 $(function() {
 
+  $("#loading").hide();
+
+  $(document).ajaxStart(function(){
+    $("#loading").show();
+
+   });
+   $(document).ajaxComplete(function(){
+     $("#loading").hide();
+   });
+
+
 
   $("#favorites").on("click", function(){
+    $("#loading").show();
     console.log("Favorites was clicked.");
     getUsers("favorite");
+
   });
   $("#online").on("click", function(){
+    $("#loading").show();
     console.log("Online was clicked.");
     getUsers("online");
 
   });
   $("#offline").on("click", function(){
+    $("#loading").show();
     console.log("Offline was clicked.");
     getUsers("offline");
   });
+
+
+
 
 
   var getUsers = function(selector){
@@ -57,6 +75,8 @@ $(function() {
 
 
       var getFaveProfiles = function(userName, streamData){
+
+
         $.ajax({
           type        : 'GET',
           url         :  "https://wind-bow.gomix.me/twitch-api/users/" + userName,
@@ -91,10 +111,13 @@ $(function() {
             var bio = "We can't find the account you're looking for.";
           }
 
-          var $newdiv1 = $( "<a href='" + "https://www.twitch.tv/" + name + "'><div class='twitchtile-offline col-md-6'><img class = 'usericon' src='" + logo + "'>" + "<h1>" + name + "</h1>" + "<h2>" + isStreaming + "</h2>" +  "<p>"  + bio + "</p>" + "</div></a>" );
+          var $newdiv1 = $( "<a href='" + "https://www.twitch.tv/" + name + "'><div class='twitchtile-offline col-md-4'><img class = 'usericon' src='" + logo + "'>" + "<h1>" + name + "</h1>" + "<h2>" + isStreaming + "</h2>" +  "<p>"  + bio + "</p>" + "</div></a>" );
           $( "#twitch-boxes" ).append( $newdiv1 ).hide().fadeIn(500);
 
           $($newdiv1).attr("id", userData.display_name);
+
+          $("#loading").hide();
+
 
           $( "#twitch-boxes" ).append( $newdiv1 ).hide().fadeIn(500);
 
@@ -137,12 +160,12 @@ $(function() {
   var generateHTML = function(streamData, userData){
 
 
-    var $newdiv1 = $( "<a href='" + "https://www.twitch.tv/" + name + "'><div class='twitchtile col-md-6'><img class = 'usericon' src='" + userData.logo + "'>" + "<h1>" + userData.display_name + "</h1>" + "<h2>Streaming: " + streamData.stream.game + "</h2>" + "<p>"  + streamData.stream.channel.status + "</p>" +
-    "<img class = 'userpreview' src='" + streamData.stream.preview.medium + "'>" + "</div></a>" );
+    var $newdiv1 = $( "<a href='" + "https://www.twitch.tv/" + name + "'><div class='twitchtile col-md-4'><img class = 'usericon' src='" + userData.logo + "'>" + "<h1>" + userData.display_name + "</h1>" + "<h2>Streaming: " + streamData.stream.game + "</h2>" + "<p>"  + streamData.stream.channel.status + "</p>" +
+    "<img class ='userpreview' src='" + streamData.stream.preview.medium + "'>" + "</div></a>" );
     $( "#twitch-boxes" ).append( $newdiv1 ).hide().fadeIn(500);
 
     $($newdiv1).attr("id", userData.display_name);
-
+    $("#loading").hide();
     $( "#twitch-boxes" ).append( $newdiv1 ).hide().fadeIn(500);
 
   }
@@ -198,8 +221,10 @@ var getOfflineProfiles = function(userName, streamData){
       var bio = "We can't find the account you're looking for.";
     }
 
-    var $newdiv1 = $( "<a href='" + "https://www.twitch.tv/" + name + "'><div class='twitchtile-offline col-md-6'><img class = 'usericon' src='" + logo + "'>" + "<h1>" + name + "</h1>" + "<h2>" + isStreaming + "</h2>" +  "<p>"  + bio + "</p>" + "</div></a>" );
+    var $newdiv1 = $( "<a href='" + "https://www.twitch.tv/" + name + "'><div class='twitchtile-offline col-md-4'><img class = 'usericon' src='" + logo + "'>" + "<h1>" + name + "</h1>" + "<h2>" + isStreaming + "</h2>" +  "<p>"  + bio + "</p>" + "</div></a>" );
     $( "#twitch-boxes" ).append( $newdiv1 ).hide().fadeIn(500);
+    $("#loading").hide();
+
 
   }
 });
